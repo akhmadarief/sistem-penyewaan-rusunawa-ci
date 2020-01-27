a99e36263923d63e2ecc57e0af193ea85c44cce4
rizqialfani01 <rizqialfani01@gmail.com>
2020-01-27 20:07:31 +0700

    Fix 404 Title Page

--- application/controllers/error404.php
+++ application/controllers/error404.php
@@ -4,7 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class error404 extends CI_Controller {
 
     public function index(){
-        $this->load->view('_partials/v_head');
+        $data['judul_halaman'] = 'Halaman Tidak Ditemukan';
+        $this->load->view('_partials/v_head', $data);
         $this->load->view('errors/404.php');
         $this->load->view('_partials/v_preloader');
         $this->load->view('_partials/v_js');
