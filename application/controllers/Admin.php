f3dbb0aa6b9205dc58df305647725f9f21ba6b2c
yogagrmy99 <yoga_doom99@yahoo.co.id>
2020-02-16 20:42:10 +0700

    Revert "edit penghuni"
    This reverts commit 75aa9fe3fe5ce7592cd860c317ae0ac8bdf85e75.

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Aksi extends CI_Controller {
+class Admin extends CI_Controller {
 
     function __construct(){
         parent::__construct();
@@ -11,251 +11,206 @@ class Aksi extends CI_Controller {
         $this->load->model('m_data');
     }
 
-    function get_prodi(){
-        $id_fakultas =  $this->input->post('id_fakultas');
-        $prodi = $this->m_data->data_prodi_by_id_fakultas($id_fakultas);
+    function index(){
+        $data['a']=$this->m_data->jumlah_penghuni_gedung('A');
+        $data['b']=$this->m_data->jumlah_penghuni_gedung('B');
+        $data['c']=$this->m_data->jumlah_penghuni_gedung('C');
+        $data['d']=$this->m_data->jumlah_penghuni_gedung('D');
+        $data['e']=$this->m_data->jumlah_penghuni_gedung('E');
 
-        echo '<option></option>';
-        foreach ($prodi->result() as $prodi){
-            echo '<option value="'.$prodi->id_prodi.'">'.$prodi->nama_prodi.'</option>';
-        }
+        $data['judul_halaman'] = 'Dasbor';
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        //$this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_dasbor'); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
     }
 
-    function get_kamar(){
-        $lantai = $this->input->post('lantai');
-        $kamar = $this->m_data->data_kamar_by_lantai($lantai);
+    function dasbor(){
+        $data['a']=$this->m_data->jumlah_penghuni_gedung('A');
+        $data['b']=$this->m_data->jumlah_penghuni_gedung('B');
+        $data['c']=$this->m_data->jumlah_penghuni_gedung('C');
+        $data['d']=$this->m_data->jumlah_penghuni_gedung('D');
+        $data['e']=$this->m_data->jumlah_penghuni_gedung('E');
 
-        echo json_encode($kamar->result());
+        $data['judul_halaman'] = 'Dasbor';
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        //$this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_dasbor'); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
     }
 
-    function get_detail_kamar(){
-        $no_kamar = $this->input->post('no_kamar');
-        $detail_kamar = $this->m_data->data_penghuni_by_kamar($no_kamar);
-
-        echo json_encode ($detail_kamar->result());
+    function pilih_kamar(){
+        $data['judul_halaman'] = 'Pilih Kamar';
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_pilih_kamar'); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
     }
 
-    function aksi_tambah_penghuni(){
-        $no_kamar       = $this->input->post('no_kamar');
-        $isi_kamar      = $this->input->post('isi_kamar');
-        $nama           = $this->input->post('nama');
-        $nim            = $this->input->post('nim');
-        $id_fakultas    = $this->input->post('id_fakultas');
-        $id_prodi       = $this->input->post('id_prodi');
-        $tempat_lahir   = $this->input->post('tempat_lahir');
-        $tgl_lahir      = $this->input->post('tgl_lahir');
-        $agama          = ($this->input->post('agama') != 'other') ? $this->input->post('agama') : $this->input->post('agama_lainnya');
-        $alamat         = $this->input->post('alamat');
-        $no             = $this->input->post('no');
-        $nama_ortu      = $this->input->post('nama_ortu');
-        $pekerjaan_ortu = $this->input->post('pekerjaan_ortu');
-        $alamat_ortu    = $this->input->post('alamat_ortu');
-        $no_ortu        = $this->input->post('no_ortu');
-        $kategori       = $this->input->post('kategori');
-        $tgl_masuk      = $this->input->post('tgl_masuk');
-        $tgl_keluar     = $this->input->post('tgl_keluar');
-        $masa_huni      = $this->input->post('masa_huni');
-        $biaya          = $this->input->post('biaya');
-        $bayar          = $this->input->post('bayar');
-        $piutang        = $this->input->post('piutang');
+    function tambah_penghuni($no_kamar = null){
 
-        $data = array(
-            'no_kamar'      => $no_kamar,
-            'isi_kamar'     => $isi_kamar,
-            'nama'          => $nama,
-            'nim'           => $nim,
-            'id_fakultas'   => $id_fakultas,
-            'id_prodi'      => $id_prodi,
-            'tempat_lahir'  => $tempat_lahir,
-            'tgl_lahir'     => $tgl_lahir,
-            'agama'         => $agama,
-            'alamat'        => $alamat,
-            'no'            => $no,
-            'nama_ortu'     => $nama_ortu,
-            'pekerjaan_ortu'=> $pekerjaan_ortu,
-            'alamat_ortu'   => $alamat_ortu,
-            'no_ortu'       => $no_ortu,
-            'kategori'      => $kategori,
-            'tgl_masuk'     => $tgl_masuk,
-            'tgl_keluar'    => $tgl_keluar,
-            'biaya'         => $biaya,
-            'status'        => 'Penghuni'
-        );
+        if (!isset($no_kamar)) redirect('admin/pilih_kamar');
 
-        $data_pembayaran = array(
-            'nim'           => $nim,
-            'tgl_bayar'     => $tgl_masuk,
-            'bayar'         => $bayar
-        );
+        $cek_kamar = $this->m_data->cek_kamar($no_kamar)->row();
 
-        $kamar = $this->m_data->cek_kamar($no_kamar)->row();
+        if (!$cek_kamar) show_404();
 
-        if ($kamar->status == 'sendiri' or $kamar->status == 'terisi2'){
-            echo '<script>alert ("Kamar sudah terisi penuh, silakan pilih kamar lain"); window.location="'.base_url('admin/pilih_kamar').'";</script>';
-        }
-        else if ($this->m_data->insert_penghuni($data) == true){
+        else if (strpos($cek_kamar->status, 'terisi2') !== false or strpos($cek_kamar->status, 'sendiri') !== false) echo '<script>alert ("Kamar sudah terisi penuh, silakan pilih kamar lain"); window.location="'.base_url('admin/pilih_kamar').'";</script>';
 
-            if ($kamar->status == 'kosong' and $isi_kamar == '2') $status_kamar = 'terisi1';
+        $data['harga_kamar'] = $this->m_data->data_harga_kamar_by_lantai($cek_kamar->lantai)->row();
+        $data['no_kamar'] = $no_kamar;
+        $data['status_kamar'] = $cek_kamar->status;
+        $data['judul_halaman'] = 'Tambah Penghuni';
+        $data['fakultas'] = $this->m_data->data_fakultas()->result();
 
-            else if ($kamar->status == 'kosong' and $isi_kamar == '1') $status_kamar = 'sendiri';
-
-            else if ($kamar->status == 'terisi1') $status_kamar = 'terisi2';
-
-            else if ($kamar->status == 'terisi1 piutang') $status_kamar = 'terisi2 piutang';
-
-            if ($piutang != 0) $status_kamar = $status_kamar.' piutang';
-
-            $this->m_data->update_status_kamar($no_kamar, $status_kamar);
-            $this->m_data->insert_pembayaran($data_pembayaran);
-
-            //redirect('admin/pilih_kamar');
-            echo 'berhasil disimpan gan';
-            //redirect('admin/pilih_kamar');
-        }
-        else {
-            echo 'gagal disimpan gan :(';
-        }
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_tambah_penghuni', $data); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
     }
 
-    function aksi_edit_penghuni(){
-        $id             = $this->input->post('id');
-        $no_kamar       = $this->input->post('no_kamar');
-        //echo $no_kamar; exit;
-        $isi_kamar      = $this->input->post('isi_kamar');
-        $nama           = $this->input->post('nama');
-        $nim            = $this->input->post('nim');
-        $id_fakultas    = $this->input->post('id_fakultas');
-        $id_prodi       = $this->input->post('id_prodi');
-        $tempat_lahir   = $this->input->post('tempat_lahir');
-        $tgl_lahir      = $this->input->post('tgl_lahir');
-        $agama          = ($this->input->post('agama') != 'other') ? $this->input->post('agama') : $this->input->post('agama_lainnya');
-        $alamat         = $this->input->post('alamat');
-        $no             = $this->input->post('no');
-        $nama_ortu      = $this->input->post('nama_ortu');
-        $pekerjaan_ortu = $this->input->post('pekerjaan_ortu');
-        $alamat_ortu    = $this->input->post('alamat_ortu');
-        $no_ortu        = $this->input->post('no_ortu');
-        $tgl_masuk      = $this->input->post('tgl_masuk');
-        $tgl_keluar     = $this->input->post('tgl_keluar');
-        $kategori       = $this->input->post('kategori');
-        $biaya          = $this->input->post('biaya'); //belum dipakai
-        $bayar          = $this->input->post('bayar');
-        $piutang        = $this->input->post('piutang'); //belum dipakai
-        $pilihan        = $this->input->post('pilihan1');
-        
-
-
-        $data = array(
-            'no_kamar'      => $no_kamar,
-            'isi_kamar'     => $isi_kamar,
-            'nama'          => $nama,
-            'nim'           => $nim,
-            'id_fakultas'   => $id_fakultas,
-            'id_prodi'      => $id_prodi,
-            'tempat_lahir'  => $tempat_lahir,
-            'tgl_lahir'     => $tgl_lahir,
-            'agama'         => $agama,
-            'alamat'        => $alamat,
-            'no'            => $no,
-            'nama_ortu'     => $nama_ortu,
-            'pekerjaan_ortu'=> $pekerjaan_ortu,
-            'alamat_ortu'   => $alamat_ortu,
-            'no_ortu'       => $no_ortu,
-            'tgl_masuk'     => $tgl_masuk,
-            'tgl_keluar'    => $tgl_keluar,
-            'kategori'      => $kategori
-        );
-
-        $data_pembayaran = array(
-            'nim'           => $nim,
-            'tgl_bayar'     => date("d-m-Y"),
-            'bayar'         => $bayar
-        );
-
-        $data_pindah_kamar = array(
-            'no_kamar'      => $nim
-        );
-
-        switch ($pilihan) {
-            case "typo":
-                $this->m_data->update_penghuni($id, $data);
-                redirect('admin/daftar_penghuni');
-                //echo $this->db->last_query();
-                //exit;
-            break;
-
-            case "transaksi":
-                $this->m_data->update_penghuni($id, $data);
-                $this->m_data->insert_pembayaran($data_pembayaran);
-                redirect('admin/daftar_penghuni');
-            break;
-
-            case "pk":              
-                $this->m_data->update_penghuni($id, $data_pindah_kamar);
-                redirect('admin/daftar_penghuni');
-            break;
-            default:
-            echo "error gan :/";exit;
-        break;
-        }
-
-        
-//ini baru
-        //$this->m_data->update_penghuni($id, $data);
-
-//sampai sini
-
-
-        // if ($this->m_data->update_penghuni($id, $data) == true){
-        //     //redirect('admin/daftar_penghuni');
-        //     echo 'berhasil diupdate gan';
-        //     redirect('admin/daftar_penghuni');
-        // }
-        // else {
-        //     echo 'gagal gan :(';
-        // }
-    }
-
-    function aksi_hapus_penghuni($id = null){
+    function edit_penghuni($id = null){
 
         if (!isset($id)) redirect('admin/daftar_penghuni');
 
-        $penghuni = $this->m_data->data_penghuni_by_id($id)->row();
+        $data['penghuni'] = $this->m_data->data_penghuni_by_id($id)->row();
 
-        if (!$penghuni){
-            show_404();
-        }
-        else if ($this->m_data->delete_penghuni($id) == true){
+        if (!$data['penghuni']) show_404();
 
-            $no_kamar = $penghuni->no_kamar;
-            $kamar = $this->m_data->cek_kamar($no_kamar)->row();
+        $id_fakultas = $data['penghuni']->id_fakultas;
 
-            if ($kamar->status == 'sendiri' or $kamar->status == 'sendiri piutang') $status_kamar = 'kosong';
+        $data['prodi'] = $this->m_data->data_prodi_by_id_fakultas($id_fakultas)->result();
+        $data['prodi1'] = $this->m_data->data_prodi_by_id_fakultas($id_fakultas)->result();
+        $data['judul_halaman'] = 'Edit Penghuni';
+        $data['fakultas'] = $this->m_data->data_fakultas()->result();
+        $data['fakultas1'] = $this->m_data->data_fakultas()->result();
 
-            else if ($kamar->status == 'terisi2') $status_kamar = 'terisi1';
-
-            else if ($kamar->status == 'terisi1') $status_kamar = 'kosong';
-
-            //menghapus penghuni berpiutang
-            else if ($kamar->status == 'terisi2 piutang') $status_kamar = 'terisi1';
-
-            else if ($kamar->status == 'terisi1 piutang') $status_kamar = 'kosong';
-
-            else if ($kamar->status == 'terisi2 piutang piutang') $status_kamar = 'terisi1 piutang';
-
-            $this->m_data->update_status_kamar($no_kamar, $status_kamar);
-
-            //redirect('admin/daftar_penghuni');
-            echo 'berhasil dihapus gan';
-        }
-        else {
-            echo 'gagal gan :(';
-        }
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_edit_penghuni', $data); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
     }
 
-    function detail_penghuni(){
-        $id = $this->input->post('id_penghuni');
-        $penghuni = $this->m_data->data_penghuni_by_id($id)->row();
-            echo json_encode($penghuni);
+    function daftar_kamar(){
+        $data['judul_halaman'] = 'Daftar Kamar';
+        $data['kamar'] = $this->m_data->data_kamar()->result();
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_daftar_kamar', $data); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
+    }
+
+    function daftar_harga(){
+        $data['judul_halaman'] = 'Daftar Harga Kamar';
+        $data['daftar_harga'] = $this->m_data->data_harga_kamar()->result();
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_daftar_harga', $data); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
+    }
+
+    function daftar_penghuni(){
+        $data['judul_halaman'] = 'Daftar Penghuni';
+        $data['penghuni'] = $this->m_data->data_penghuni()->result();
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_daftar_penghuni', $data); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
+    }
+
+    function riwayat_pembayaran(){
+        $data['judul_halaman'] = 'Riwayat Pembayaran';
+        $data['pembayaran'] = $this->m_data->data_pembayaran()->result();
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_riwayat_pembayaran', $data); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
+    }
+
+    function laporan_keuangan(){
+        $data['judul_halaman'] = 'Laporan Keuangan';
+        $data['keuangan'] = $this->m_data->data_keuangan_per_penghuni()->result();
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_laporan_keuangan', $data); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
+    }
+
+    function laporan_piutang(){
+        $data['judul_halaman'] = 'Laporan Piutang';
+        $data['penghuni'] = $this->m_data->data_penghuni()->result();
+        $data['keuangan1'] = $this->m_data->data_keuangan_per_penghuni()->result();
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_laporan_piutang', $data); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
+    }
+
+    function kosong(){
+        $data['judul_halaman'] = 'Kosong';
+        $this->load->view('_partials/v_head', $data);
+        $this->load->view('_partials/v_header');
+        $this->load->view('_partials/v_sidebar', $data);
+        $this->load->view('_partials/v_breadcrump', $data);
+        $this->load->view('v_kosong'); //page content
+        $this->load->view('_partials/v_footer');
+        $this->load->view('_partials/v_theme-config');
+        $this->load->view('_partials/v_preloader');
+        $this->load->view('_partials/v_js');
     }
 }
