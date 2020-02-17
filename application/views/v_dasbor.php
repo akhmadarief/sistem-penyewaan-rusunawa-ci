46a980fa5933b15291b7775c91cb89db0b371e0d
rizqialfani01 <rizqialfani01@gmail.com>
2020-02-18 03:02:08 +0700

    fixing summary gedung

--- application/views/v_dasbor.php
+++ application/views/v_dasbor.php
@@ -43,9 +43,9 @@
                 <div class="row">
                     <div class="col-lg-3 col-md-6">
                         <div class="ibox bg-primary color-white widget-stat" style="background-color: #3498db;">
-                            <div class="ibox-body">
+                            <div class="ibox-body sum-kamar">
                                 <h4 class="m-b-5 font-strong">Gedung A</h4>
-                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (84 - $a['terisi2']) ?> Kamar</div><i class="widget-stat-icon"><img src="<?php echo base_url("assets/img/A.png"); ?>"/></i>
+                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (84 - $a['terisi2']) ?> Kamar</div><i class="widget-stat-icon icon-gedung"><img src="<?php echo base_url("assets/img/A.png"); ?>"/></i>
                                 <div class="m-b-5"><label class="sum-gedung">Bisa diisi 2 Orang</label>: <?php echo (84 - $a['terisi2'] - $a['terisi1']) ?> Kamar</div>
                                 <div class="m-b-5"><label class="sum-gedung">Total Kamar </label>: 84 Kamar</div>
                             </div>
@@ -53,9 +53,9 @@
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="ibox bg-primary color-white widget-stat" style="background-color: #3498db;">
-                            <div class="ibox-body">
+                            <div class="ibox-body sum-kamar">
                                 <h4 class="m-b-5 font-strong">Gedung B</h4>
-                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (97 - $b['terisi2']) ?> Kamar</div><i class="widget-stat-icon"><img src="<?php echo base_url("assets/img/B.png"); ?>"/></i>
+                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (97 - $b['terisi2']) ?> Kamar</div><i class="widget-stat-icon icon-gedung"><img src="<?php echo base_url("assets/img/B.png"); ?>"/></i>
                                 <div class="m-b-5"><label class="sum-gedung">Bisa diisi 2 Orang</label>: <?php echo (97 - $b['terisi2'] - $b['terisi1']) ?> Kamar</div>
                                 <div class="m-b-5"><label class="sum-gedung">Total Kamar </label>: 97 Kamar</div>
                             </div>
@@ -63,9 +63,9 @@
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="ibox bg-primary color-white widget-stat" style="background-color: #3498db;">
-                            <div class="ibox-body">
+                            <div class="ibox-body sum-kamar">
                                 <h4 class="m-b-5 font-strong">Gedung C</h4>
-                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (96 - $c['terisi2']) ?> Kamar</div><i class="widget-stat-icon"><img src="<?php echo base_url("assets/img/C.png"); ?>"/></i>
+                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (96 - $c['terisi2']) ?> Kamar</div><i class="widget-stat-icon icon-gedung"><img src="<?php echo base_url("assets/img/C.png"); ?>"/></i>
                                 <div class="m-b-5"><label class="sum-gedung">Bisa diisi 2 Orang</label>: <?php echo (96 - $c['terisi2'] - $c['terisi1']) ?> Kamar</div>
                                 <div class="m-b-5"><label class="sum-gedung">Total Kamar </label>: 96 Kamar</div>
                             </div>
@@ -73,9 +73,9 @@
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="ibox bg-primary color-white widget-stat" style="background-color: #3498db;">
-                            <div class="ibox-body">
+                            <div class="ibox-body sum-kamar">
                                 <h4 class="m-b-5 font-strong">Gedung D</h4>
-                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (103 - $d['terisi2']) ?> Kamar</div><i class="widget-stat-icon"><img src="<?php echo base_url("assets/img/D.png"); ?>"/></i>
+                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (103 - $d['terisi2']) ?> Kamar</div><i class="widget-stat-icon icon-gedung"><img src="<?php echo base_url("assets/img/D.png"); ?>"/></i>
                                 <div class="m-b-5"><label class="sum-gedung">Bisa diisi 2 Orang</label>: <?php echo (103 - $d['terisi2'] - $d['terisi1']) ?> Kamar</div>
                                 <div class="m-b-5"><label class="sum-gedung">Total Kamar </label>: 103 Kamar</div>
                             </div>
@@ -83,9 +83,9 @@
                     </div>
                     <div class="col-lg-3 col-md-6">
                         <div class="ibox bg-primary color-white widget-stat" style="background-color: #3498db;">
-                            <div class="ibox-body">
+                            <div class="ibox-body sum-kamar">
                                 <h4 class="m-b-5 font-strong">Gedung E</h4>
-                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (114 - $e['terisi2']) ?> Kamar</div><i class="widget-stat-icon"><img src="<?php echo base_url("assets/img/E.png"); ?>"/></i>
+                                <div class="m-b-5"><label class="sum-gedung">Bisa diisi 1 Orang</label>: <?php echo (114 - $e['terisi2']) ?> Kamar</div><i class="widget-stat-icon icon-gedung"><img src="<?php echo base_url("assets/img/E.png"); ?>"/></i>
                                 <div class="m-b-5"><label class="sum-gedung">Bisa diisi 2 Orang</label>: <?php echo (114 - $e['terisi2'] - $e['terisi1']) ?> Kamar</div>
  <!--suatu saat dibutuhkan      <div class="m-b-5"><label class="sum-gedung">Sendiri </label>: <?php echo $e['sendiri'] ?> Kamar</div> -->
                                 <div class="m-b-5"><label class="sum-gedung">Total Kamar </label>: 114 Kamar</div>
