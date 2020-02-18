8db508351b925f4f36bd8fa2de3facb8d1f45759
sepatah <38358486+sepatah@users.noreply.github.com>
2020-02-18 21:23:38 +0700

    slideshow dasbor

--- application/views/v_dasbor.php
+++ application/views/v_dasbor.php
@@ -11,30 +11,59 @@
         <?php //$this->load->view('_partials/sidebar.php') ?>
         <!-- END SIDEBAR-->
         <div style="position:relative; font-size:25px; text-align:center;">
-
         </div>
         <div class="st">
-          <div class="slideshow-container">
-
-          <div class="mySlides fade">
-            <img src="<?php echo base_url('assets/img/a.jpg')  ?>" style="width:100%">
+          <!--Carousel Wrapper-->
+          <div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+          <!--Slides-->
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <div class="view">
+                <img class="d-block w-100" src="<?php echo base_url('assets/img/a.jpg')  ?>"
+                  alt="First slide">
+                <div class="mask rgba-black-light"></div>
+              </div>
+              <div class="carousel-caption">
+                <h3 class="h3-responsive">Light mask</h3>
+                <p>First text</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <!--Mask color-->
+              <div class="view">
+                <img class="d-block w-100" src="<?php echo base_url('assets/img/rusunawa01.jpg')  ?>"
+                  alt="Second slide">
+                <div class="mask rgba-black-strong"></div>
+              </div>
+              <div class="carousel-caption">
+                <h3 class="h3-responsive">Strong mask</h3>
+                <p>Secondary text</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <!--Mask color-->
+              <div class="view">
+                <img class="d-block w-100" src="<?php echo base_url('assets/img/rusunawa02.jpg')  ?>"
+                  alt="Third slide">
+                <div class="mask rgba-black-slight"></div>
+              </div>
+              <div class="carousel-caption">
+                <h3 class="h3-responsive">Slight mask</h3>
+                <p>Third text</p>
+              </div>
+            </div>
           </div>
-
-          <div class="mySlides fade">
-            <img src="<?php echo base_url('assets/img/rusunawa01.jpg')  ?>" style="width:100%">
-          </div>
-
-          <div class="mySlides fade">
-            <img src="<?php echo base_url('assets/img/rusunawa02.jpg')  ?>" style="width:100%">
-          </div>
-
-          </div>
-          <br>
-
-        <div style="text-align:center">
-            <span class="dot"></span>
-            <span class="dot"></span>
-            <span class="dot"></span>
+          <!--/.Slides-->
+          <!--Controls-->
+          <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+          <!--/.Controls-->
           </div>
         </div>
 
