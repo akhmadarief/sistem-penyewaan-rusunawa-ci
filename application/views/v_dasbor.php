3d9c72b8973d92ec608a94133ad1f6b232021c42
sepatah <38358486+sepatah@users.noreply.github.com>
2020-02-18 21:31:29 +0700

    Update v_dasbor.php

--- application/views/v_dasbor.php
+++ application/views/v_dasbor.php
@@ -14,7 +14,7 @@
         </div>
         <div class="st">
           <!--Carousel Wrapper-->
-          <div id="carousel-example-2" class="carousel slide carousel-fade" data-ride="carousel">
+          <div id="carousel-slider" class="carousel slide carousel-fade" data-ride="carousel">
           <!--Slides-->
           <div class="carousel-inner" role="listbox">
             <div class="carousel-item active">
@@ -55,11 +55,11 @@
           </div>
           <!--/.Slides-->
           <!--Controls-->
-          <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
+          <a class="carousel-control-prev" href="#carousel-slider" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
           </a>
-          <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
+          <a class="carousel-control-next" href="#carousel-slider" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
           </a>
