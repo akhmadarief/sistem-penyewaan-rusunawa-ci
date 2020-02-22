fae725be616f9dfbe70c59fb1c512e52f2741a15
yogagrmy99 <yoga_doom99@yahoo.co.id>
2020-02-22 15:50:14 +0700

    ngablu tidak benar

--- application/config/database.php
+++ application/config/database.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-
+/*
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'rusunawa.reg.my.id',
@@ -95,8 +95,8 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+*/
 
-/*
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
@@ -118,4 +118,3 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
-*/
