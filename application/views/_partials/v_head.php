33e8e5b06af3c7c4d53b25f998d39d82f32100d5
yogagrmy99 <yoga_doom99@yahoo.co.id>
2020-02-25 02:01:09 +0700

    Update v_head.php

--- application/views/_partials/v_head.php
+++ application/views/_partials/v_head.php
@@ -25,4 +25,25 @@
     <!-- SLIDESHOW STYLES-->
     <!-- PRINT STYLES-->
     <!--<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" rel="stylesheet">-->
+    <style>
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #cfcfcf; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
+</style>
 </head>
