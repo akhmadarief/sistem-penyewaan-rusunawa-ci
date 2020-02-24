48617d9c85d38984f9f8de5616dba86ddd9529b9
rizqialfani01 <rizqialfani01@gmail.com>
2020-02-24 15:56:31 +0700

    alert fade

--- application/views/_partials/v_js_form.php
+++ application/views/_partials/v_js_form.php
@@ -84,6 +84,15 @@
             });
         });
     </script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            window.setTimeout(function() {
+                $(".alert").fadeTo(500, 0).slideUp(500, function(){
+                    $(this).remove(); 
+                });
+            }, 2000);
+        });
+    </script>
 </body>
 
 </html>
