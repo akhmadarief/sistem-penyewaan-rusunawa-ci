7db83b12ee55cbb447e86d69dd931b99ba70e9f8
akhmadarief <38371717+akhmadarief@users.noreply.github.com>
2020-03-18 23:06:49 +0700

    git ignore

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
+
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'rusunaw4',
-	'password' => 'gVUIk67kGJhjkHYUG786hgjfgj',
-	'database' => 'ru5un4wa',
+	'hostname' => '13.67.111.168',
+	'username' => 'rusunawa',
+	'password' => 'qwerty123',
+	'database' => 'rusunawa',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -94,3 +95,27 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+/*
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'rusunawa',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => FALSE,
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+*/
